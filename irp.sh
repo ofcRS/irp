f08e1a40194f479cce8afe00bce23b03add04d9a
aleksander.sakhtakiy <azeric054@gmail.com>
2022-10-16 04:36:25 +0400

    add dependencies list to install

--- irp.sh
+++ irp.sh
@@ -1,7 +1,9 @@
 yarn init -y
 
+yarn add $(tr -s '[:space:]' ' ' < "$(dirname "$(readlink -f "$0")")/dependencies-list")
+
 touch .gitignore
 echo "node_modules/*" >> .gitignore
 echo "webpack/*" >> .gitignore
 
-node "$(dirname "$(readlink -f "$0")")/initializeConfigs.mjs"
+node "$(dirname "$(readlink -f "$0")")/initConfigs.mjs"
